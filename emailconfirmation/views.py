07de6877d43530e1f9c87e50d225d11250833bb5
Jose Penarrieta <soad241@gmail.com>
2010-08-16 11:42:15 -0500

    fixing success message

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from emailconfirmation.models import EmailConfirmation
 
 def confirm_email(request, confirmation_key):
     confirmation_key = confirmation_key.lower()
     email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
-    return render_to_response("emailconfirmation/confirm_email.html", {
-        "email_address": email_address,
-    }, context_instance=RequestContext(request))
+    messages.add_message(request, messages.INFO,
+                         'Congrats, your email is confirmed!')
+    return HttpResponseRedirect('/')
